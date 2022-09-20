cd21a85faf18ae7984d685d8a215457d8c605a94
Gabriel <a01701482@tec.mx>
2022-09-20 13:58:05 -0500

    Agregar Password en Base de Datos dependiendo de la compu

--- util/database.js
+++ util/database.js
@@ -1,10 +1,18 @@
 const mysql = require("mysql2");
 
+const ENV = "GABRIEL";
+
+let password = '';
+if (ENV == 'DENISSE') {
+  password = 'root'
+}
+
+
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "reportes_st",
-  password: "",
+  password: password,
 });
 
 module.exports = pool.promise();
