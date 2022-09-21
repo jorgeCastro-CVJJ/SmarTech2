1a66d79d4b39ab37d45f9003540abce3963f7d11
Fer Torres <fertorres638gmail.com>
2022-09-20 21:52:31 -0500

    crear modelo tareas

--- models/verTareas.js
+++ models/verTareas.js
@@ -2,3 +2,8 @@ const db = require("../util/database");
 const bcrypt = require("bcryptjs");
 
 //crear el objeto
+module.exports = class Tareas {
+    constructor(nombreT, idTarea, horasTrabajo, idProyecto) {
+        
+    }
+}
