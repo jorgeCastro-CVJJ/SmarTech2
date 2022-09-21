cfca246483107eecb096ef214a1f18c9d33730f9
Denisse Dominguez <A01702603@itesm.mx>
2022-09-21 14:33:28 -0500

    correcto save en base de datos

--- models/proyectoModel.js
+++ models/proyectoModel.js
@@ -3,18 +3,17 @@ const bcrypt = require("bcryptjs");
 
 module.exports = class Proyecto {
   
-  constructor(idProyecto, nombreP, estatus, descripcion, stackTecnologico, stakeholders){
-    this.idProyecto = idProyecto;
+  constructor(nombreP, descripcion, estatus, stackTecnologico, stakeholders){
     this.nombreP = nombreP;
     this.descripcion = descripcion;
+    this.estatus = estatus;
     this.stackTecnologico = stackTecnologico;
     this.stakeholders = stakeholders;
-    this.estatus = estatus;
   }
 
   // hacer un nuevo proyecto, insert en la base de datos
   save(){
-    return db.execute('INSERT INTO proyecto (nombreP, descripcion, stackTecnologico, stakeholders, estatus) VALUES (?,?,?,?,?)', [this.nombreP, this.descripcion, this.stackTecnologico, this.stakeholders, this.estatus]);
+    return db.execute('INSERT INTO proyecto (nombreP, descripcion, estatus, stackTecnologico, stakeholders) VALUES (?,?,?,?,?)', [this.nombreP, this.descripcion, this.estatus, this.stackTecnologico, this.stakeholders]);
   }
 
   // jalar todo de la base de datos, 
