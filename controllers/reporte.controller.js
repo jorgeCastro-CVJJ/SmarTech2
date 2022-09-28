6e64931b6deb353871428291f83f46cdf3b8961a
Gabriel <a01701482@tec.mx>
2022-09-27 19:21:19 -0500

    Reporte Controlador 2

---
+++ controllers/reporte.controller.js
@@ -0,0 +1,5 @@
+const path = require("path");
+const Proyecto = require("../models/proyectoModel");
+const Tarea  = require("../models/tareaModel");
+const Empleado = require("../models/empleadoModel");
+const Usuario = require("../models/usuarioModel");
