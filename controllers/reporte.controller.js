69f372d6f557a781dfe055ba960cc90da4d24a66
Gabriel <a01701482@tec.mx>
2022-09-27 20:07:50 -0500

    Reporte controlador

--- controllers/reporte.controller.js
+++ controllers/reporte.controller.js
@@ -3,3 +3,12 @@ const Proyecto = require("../models/proyectoModel");
 const Tarea  = require("../models/tareaModel");
 const Empleado = require("../models/empleadoModel");
 const Usuario = require("../models/usuarioModel");
+const Reporte = require("../models/reporteModel");
+
+/*postnuevoReporte = (request, response, next) => {
+    const nuevoReporte = new Reporte(request.body.proporcion, request.body.personalCompleto, request.body.personalMedio, request.body.horasVacaciones, request.body.horasModificadas);
+    nuevoReporte.save()
+    .then(()=> {
+        Reporte.
+    })
+}*/
