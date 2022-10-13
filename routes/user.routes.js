8480e8debe66ce1f6d573b67cac7e1eafe990f9e
CVJJ <A01707735@itesm.mx>
2022-10-12 23:54:02 -0500

    routes created

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,11 +10,23 @@ const isAuth = require('../util/isAuth');
 router.get('/login', userController.getLogin)
 
 
+<<<<<<< Updated upstream
 router.post('/login', userController.postLogin);
+=======
+router.get("/login", userController.getLogin);
+router.post("/login", userController.postLogin);
+router.get("/logout", userController.logout)
+>>>>>>> Stashed changes
 
 router.get('/inicio', isAuth, userController.menu);
 router.post('/inicio', isAuth, userController.menu);
 
+<<<<<<< Updated upstream
 router.get('/logout', userController.logout)
+=======
+router.get('/crearUsuario',userController.getCrearUsuario);
+router.post('/crearusuario',userController.postCrearUsuario);
+
+>>>>>>> Stashed changes
 
 module.exports = router;
