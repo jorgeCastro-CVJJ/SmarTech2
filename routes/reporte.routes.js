584af8f45ba34e980433be318bf443a3249072af
Fer Torres <fertorres638gmail.com>
2022-10-14 13:22:22 -0500

    filtro reporte funciona :D

--- routes/reporte.routes.js
+++ routes/reporte.routes.js
@@ -7,4 +7,6 @@ router.get("/nuevoReporte", isAuth,reporteController.getReportes);
 router.get("/crearReporte", isAuth,reporteController.postnuevoReporte);
 router.post("/registrarReporte", isAuth,reporteController.postReporte);
 
+router.get('/buscar/:fechaInicio/:fechaFin', isAuth, reporteController.getBuscarReporte);
+
 module.exports = router;
