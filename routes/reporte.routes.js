e061de6324d25bf1b6d721cba8eeb660d31b26dc
Fer Torres <fertorres638gmail.com>
2022-10-17 23:55:15 -0500

    cambios reporte

--- routes/reporte.routes.js
+++ routes/reporte.routes.js
@@ -5,7 +5,7 @@ const isAuth = require('../util/isAuth');
 
 router.get("/nuevoReporte", isAuth,reporteController.getReportes);
 router.get("/crearReporte", isAuth,reporteController.postnuevoReporte);
-router.post("/registrarReporte", isAuth,reporteController.postReporte);
+router.post("/crearReporte", isAuth,reporteController.postReporte);
 
 router.get('/buscar/:fechaInicio/:fechaFinal', isAuth, reporteController.getBuscarReporte);
 
